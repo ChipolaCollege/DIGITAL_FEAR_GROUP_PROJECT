2ef8955a5f9f1266beeeaf6ba11e086a920c1c72
xyz4512 <chris_mister48@Yahoo.com>
2016-10-12 15:29:36 -0500

    Big Changes
    I added some more files to the game. This is mostly audio. I cleaned up
    some of the games code as well. Mostly improving the gravity.

--- GameDEV/main.js
+++ GameDEV/main.js
@@ -4,6 +4,8 @@ var game = new Phaser.Game(1080, 720, Phaser.CANVAS, 'game01v', { preload: prelo
 var cursors;
 
 function preload() {
+	game.load.audio('engine',['assets/randomize3.mp3','assets/randomize3.oog']);
+	game.load.audio('music',['assets/wicked.mp3','assets/wicked.oog'])
 	game.load.image('space','assets/starfield.png');
 	game.load.image('warship','assets/warship.png');
 }
@@ -11,10 +13,13 @@ function preload() {
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	game.add.image(0,0, 'space')
+	//music
+	music=game.add.audio("music")
+	music.play('',1,true);
 	players = game.add.group();
 	players.enableBody = true;
 
-	createPlayer(40,300);
+	createPlayer(300,400);
 
 	cursors = game.input.keyboard.createCursorKeys();
 }
@@ -26,17 +31,33 @@ function update(){
 function createPlayer(x,y){
 	var player = players.create(x,y, 'warship');
 	player.body.bounce.y =.25;
-	player.body.gravity.y = 300;
+	player.body.gravity.y = 0;
 	player.body.collideWorldBounds = true;
 }
 
 function playerUpdate(){
+	game.physics.arcade.collide(players, players)
 	players.forEach(function(p){
 		p.body.velocity.x = 0;
 		if(cursors.left.isDown){
+			var snd = game.add.audio("engine")
+			snd.play();
 			p.body.velocity.x = -150;
 		}else if(cursors.right.isDown){
+			var snd = game.add.audio("engine")
+			snd.play();
 			p.body.velocity.x = 150;
+		}
+		p.body.velocity.y = 0;
+		if(cursors.up.isDown){
+			var snd = game.add.audio("engine")
+			snd.play();
+			p.body.velocity.y = -150
+		}
+		else if(cursors.down.isDown){
+			p.body.velocity.y = 150;
+			var snd = game.add.audio("engine")
+			snd.play();
 		}
 		});
 }
